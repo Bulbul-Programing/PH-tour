e58ab7e50af09aeb998f25b6b66b3d7b6bd44ad9
Bulbul-Programing <bulbul.programing@gmail.com>
2025-07-15 22:45:18 +0600

    catch async functionality add

--- src/app/Modules/user/user.controller.ts
+++ src/app/Modules/user/user.controller.ts
@@ -1,15 +1,20 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
+import { CatchAsync } from "../../utils/catchAsync";
 
-const createUser = async (req: Request, res: Response) => {
+const createUser = CatchAsync(async (req: Request, res: Response) => {
     const user = await UserServices.createUser(req.body)
 
     res.status(httpStatus.CREATED).json({
         message: "User Created Successfully",
         user
     })
-}
+})
+
+
+
+
 
 export const UserControllers = {
     createUser
